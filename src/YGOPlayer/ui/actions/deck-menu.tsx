b453eeb35d0e8fa832aad640cbabd92405945704
Micael Sampaio <ruimicaelsampaio@gmail.com>
2025-02-16 14:04:50 +0000

    feat: add flex styles add mill buttons

--- src/YGOPlayer/ui/actions/deck-menu.tsx
+++ src/YGOPlayer/ui/actions/deck-menu.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useLayoutEffect, useRef } from "react";
+import { useCallback, useLayoutEffect, useRef, useState } from "react";
 import { YGODuel } from "../../core/YGODuel";
 import { CardMenu } from "../components/CardMenu";
 import { getTransformFromCamera } from "../../scripts/ygo-utils";
@@ -7,17 +7,15 @@ import { Deck } from "../../game/Deck";
 export function DeckMenu({ duel, deck }: { duel: YGODuel, deck: Deck, clearAction: Function, mouseEvent: React.MouseEvent }) {
     const menuRef = useRef<HTMLDivElement>(null);
 
-    // const drawFromDeck = useEffect((e: React.MouseEvent) => {
-    //     duel.ygo.exec(new YGOCommands.DrawFromDeckCommand({ player: deck.player }));
-    // }
+    const [millCounter, setMilCounter] = useState<number>(1);
 
     const drawFromDeck = useCallback(() => {
         duel.gameActions.drawFromDeck({ player: deck.player });
     }, [deck]);
 
     const milFromDeck = useCallback(() => {
-        duel.gameActions.milFromDeck({ player: deck.player });
-    }, [deck]);
+        duel.gameActions.milFromDeck({ player: deck.player, numberOfCards: millCounter });
+    }, [deck, millCounter]);
 
     const viewDeck = () => {
         duel.events.publish("toggle-ui-menu", { group: "game-popup", type: "view-main-deck", data: { duel, deck } });
@@ -34,7 +32,21 @@ export function DeckMenu({ duel, deck }: { duel: YGODuel, deck: Deck, clearActio
     const mainDeckSize = duel.ygo.state.fields[0].mainDeck.length;
 
     return <CardMenu menuRef={menuRef}>
-        <button className="ygo-card-item" disabled={mainDeckSize === 0} type="button" onClick={milFromDeck}>Mil</button>
+        <div className="ygo-d-flex ygo-gap-2">
+            <div className="ygo-flex-grow-1">
+                <button className="ygo-card-item" disabled={mainDeckSize === 0} type="button" onClick={milFromDeck}>Mil</button>
+            </div>
+            <div>
+                <button className="ygo-card-item" disabled={mainDeckSize === 0} type="button" onClick={() => setMilCounter(counter => counter > 0 ? counter - 1 : 0)}>+</button>
+            </div>
+            <div>
+                {millCounter}
+            </div>
+            <div>
+                <button className="ygo-card-item" disabled={mainDeckSize === 0} type="button" onClick={() => setMilCounter(counter => counter + 1)}>+</button>
+            </div>
+
+        </div>
         <button className="ygo-card-item" type="button" onClick={viewDeck}>View Deck</button>
         <button className="ygo-card-item" disabled={mainDeckSize === 0} type="button" onClick={drawFromDeck}>Draw</button>
     </CardMenu >
