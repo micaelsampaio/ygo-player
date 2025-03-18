78769b12dd5a5c9107dee8c19d9a6a0e71ee4e07
Micael Sampaio <ruimicaelsampaio@gmail.com>
2025-03-18 22:05:33 +0000

    feat: selected card improvements

--- src/YGOPlayer/ui/menus/select-card-menu.tsx
+++ src/YGOPlayer/ui/menus/select-card-menu.tsx
@@ -1,7 +1,8 @@
 import { YGODuel } from "../../core/YGODuel";
 import { Card } from "../../../YGOCore/types/types";
-import { useLayoutEffect, useRef } from "react";
+import { useLayoutEffect, useMemo, useRef } from "react";
 import { getTransformFromCamera } from "../../scripts/ygo-utils";
+import { YGOGameUtils } from "../../../YGOCore";
 
 export function SelectedCardMenu({ duel, card, visible = true }: { duel: YGODuel, card: Card, visible: boolean }) {
     const menuRef = useRef<HTMLDivElement>(null);
@@ -22,8 +23,38 @@ export function SelectedCardMenu({ duel, card, visible = true }: { duel: YGODuel
 
     }, [visible, menuRef.current, card, duel.duelScene.selectedCardPlaceholder]);
 
+    const cardData = useMemo(() => {
+        if (!card) return null;
+
+        let colorClassName;
+        const isMonster = YGOGameUtils.isMonster(card);
+        const isLinkMonster = YGOGameUtils.isLinkMonster(card);
+
+        if (isLinkMonster) {
+            colorClassName = "ygo-link-card-bg";
+        } else if (YGOGameUtils.isXYZMonster(card)) {
+            colorClassName = "ygo-xyz-card-bg";
+        } else if (isMonster) {
+            colorClassName = "ygo-effect-monster-card-bg";
+        } else if (YGOGameUtils.isSpell(card)) {
+            colorClassName = "ygo-spell-card-bg";
+        } else if (YGOGameUtils.isTrap(card)) {
+            colorClassName = "ygo-trap-card-bg";
+        } else {
+            colorClassName = "ygo-effect-monster-card";
+        }
+
+        return {
+            type: `[${card.typeline && card.typeline.join(" / ")}]`,
+            colorClassName,
+            isMonster,
+            isLinkMonster
+        }
+    }, [card]);
+
     if (!visible) return null;
     if (!card) return null;
+    if (!cardData) return null;
 
     return <div className="selected-card-menu" ref={menuRef} onMouseMove={(e) => {
         e.preventDefault();
@@ -35,19 +66,43 @@ export function SelectedCardMenu({ duel, card, visible = true }: { duel: YGODuel
             e.stopPropagation();
         }}
     >
-        <div style={{ fontSize: "20px" }}>
+        <div className={`ygo-selected-card-header ${cardData.colorClassName}`} style={{ fontSize: "20px" }}>
             {
                 card.name
             }
             <div style={{ fontSize: "10px" }}>{card.id}</div>
         </div>
 
-        <img className="ygo-card" style={{ objectFit: "contain" }} src={`${duel.config.cdnUrl}/images/cards_small/${card.id}.jpg`} />
+        <div className={`ygo-player-${card.owner}-bg-bottom ygo-flex`}>
+            <div>
+                <img className="ygo-card" style={{ objectFit: "contain" }} src={`${duel.config.cdnUrl}/images/cards_small/${card.id}.jpg`} />
+            </div>
+            <div>
+                {cardData.isMonster && <>
+                    <div>
+                        LVL: {card.linkval || card.level}
+                    </div>
+                    <div>
+                        ATK: {card.currentAtk}
+                    </div>
+                    <div>
+                        {!cardData.isLinkMonster && <>
+                            DEF: {card.currentDef}
+                        </>}
+                    </div>
+                </>}
+
+            </div>
+
+        </div>
+
+
+
+        <div className="mb-2 ygo-text-sm">
+            {cardData.type}
+        </div>
 
         <div className="ygo-scroll-y ygo-grow ygo-text-md">
-            <div className="mb-2 ygo-text-sm">
-                {card.typeline && card.typeline.join(" / ")}
-            </div>
             {
                 card.desc && card.desc
             }
