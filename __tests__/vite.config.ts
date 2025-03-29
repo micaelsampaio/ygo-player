84aad7c72a5b46d7ee3bcafcd4aa2c0e19aeb3d0
Ivo Ribeiro <15598128+ivoribeiro@users.noreply.github.com>
2025-03-29 22:43:20 +0100

    chore: change player path when its docker

--- __tests__/vite.config.ts
+++ __tests__/vite.config.ts
@@ -2,13 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
+// Check if running in Docker
+const isDocker = process.env.DOCKER === "true";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       path: "path-browserify",
-      "ygo-player": resolve(__dirname, "../src"),
+      // Use different paths for local and Docker builds
+      "ygo-player": isDocker
+        ? resolve(__dirname, "./node_modules/ygo-player")
+        : resolve(__dirname, "../src"),
     },
   },
   build: {
