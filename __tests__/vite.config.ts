3e54794f100cd8102337314c89b3900c580c7d2b
Ivo Ribeiro <15598128+ivoribeiro@users.noreply.github.com>
2025-03-30 12:40:11 +0200

    chore: fix local

--- __tests__/vite.config.ts
+++ __tests__/vite.config.ts
@@ -14,11 +14,11 @@ export default defineConfig({
       // Resolve CSS for ygo-player
       "ygo-player/style.css": isDocker
         ? resolve(__dirname, "./node_modules/ygo-player/style.css") // Point to style.css in Docker
-        : resolve(__dirname, "../src/style.css"), // Point to style.css for local development
+        : resolve(__dirname, "../dist/style.css"), // Point to style.css for local development
       // Resolve JavaScript (bundle.js) for ygo-player
       "ygo-player": isDocker
         ? resolve(__dirname, "./node_modules/ygo-player/bundle.js") // Point to bundle.js in Docker
-        : resolve(__dirname, "../src"), // Point to src for local development
+        : resolve(__dirname, "../dist/bundle.js"), // Point to src for local development
     },
   },
   build: {
