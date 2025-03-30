8db2dc021318141d3c65f3fad9b34175cd25f1b2
Ivo Ribeiro <15598128+ivoribeiro@users.noreply.github.com>
2025-03-30 11:38:38 +0200

    fix: change priority

--- __tests__/vite.config.ts
+++ __tests__/vite.config.ts
@@ -11,14 +11,14 @@ export default defineConfig({
   resolve: {
     alias: {
       path: "path-browserify",
-      // Resolve JavaScript (bundle.js) for ygo-player
-      "ygo-player": isDocker
-        ? resolve(__dirname, "./node_modules/ygo-player/bundle.js") // Point to bundle.js in Docker
-        : resolve(__dirname, "../src"), // Point to src for local development
       // Resolve CSS for ygo-player
       "ygo-player/style.css": isDocker
         ? resolve(__dirname, "./node_modules/ygo-player/style.css") // Point to style.css in Docker
         : resolve(__dirname, "../src/style.css"), // Point to style.css for local development
+      // Resolve JavaScript (bundle.js) for ygo-player
+      "ygo-player": isDocker
+        ? resolve(__dirname, "./node_modules/ygo-player/bundle.js") // Point to bundle.js in Docker
+        : resolve(__dirname, "../src"), // Point to src for local development
     },
   },
   build: {
