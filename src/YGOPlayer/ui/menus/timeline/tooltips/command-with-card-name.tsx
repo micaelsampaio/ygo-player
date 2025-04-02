9a3d13aa1d2954b5eb8160551b0f0105ebc31950
Ivo Ribeiro <15598128+ivoribeiro@users.noreply.github.com>
2025-04-02 10:18:17 +0200

    fix: comment MoveCardCommand type

--- src/YGOPlayer/ui/menus/timeline/tooltips/command-with-card-name.tsx
+++ src/YGOPlayer/ui/menus/timeline/tooltips/command-with-card-name.tsx
@@ -1,19 +1,22 @@
 import { memo } from "react";
 import { TimelineCommandProps } from "../../timeline";
-import { MoveCardCommand } from "ygo-core/dist/commands/MoveCardCommand";
+//import { MoveCardCommand } from "ygo-core/dist/commands/MoveCardCommand";
 
-export const TooltipCommandWithCardName = memo(function ({ command: baseCommand, duel }: TimelineCommandProps) {
-    const command = baseCommand as MoveCardCommand;
-    const card = duel.ygo.state.getCardData(command.data.id);
+export const TooltipCommandWithCardName = memo(function ({
+  command: baseCommand,
+  duel,
+}: TimelineCommandProps) {
+  const command = baseCommand as any;
+  const card = duel.ygo.state.getCardData(command.data.id);
 
-    return <div className="command-tooltip">
+  return (
+    <div className="command-tooltip">
+      <div>
         <div>
-            <div>
-                <b>{(command as any).type}</b>
-            </div>
-            <div>
-                {card?.name}
-            </div>
+          <b>{(command as any).type}</b>
         </div>
+        <div>{card?.name}</div>
+      </div>
     </div>
-})
+  );
+});
