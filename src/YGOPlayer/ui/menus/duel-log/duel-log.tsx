dc361ceb868c35d37d448c74bd7b260fe1eca98c
Micael Sampaio <ruimicaelsampaio@gmail.com>
2025-07-23 20:50:50 +0100

    feat: better duel-log buttons to undo

--- src/YGOPlayer/ui/menus/duel-log/duel-log.tsx
+++ src/YGOPlayer/ui/menus/duel-log/duel-log.tsx
@@ -105,9 +105,14 @@ export function DuelLogMenu({ duel, menus }: { duel: YGODuel; menus: any[] }) {
           );
         })}
 
-        <div onClick={undo}>
-          <button>undo</button>
-        </div>
+        {logs?.length > 0 && <div className="ygo-flex ygo-mt-2 ygo-p-2">
+          <button className="ygo-btn ygo-btn-action ygo-flex-grow-1" onClick={undo}>
+            <div>Undo Action</div>
+            <div className="ygo-btn-right-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-200v-80h284q63 0 109.5-40T720-420q0-60-46.5-100T564-560H312l104 104-56 56-200-200 200-200 56 56-104 104h252q97 0 166.5 63T800-420q0 94-69.5 157T564-200H280Z" /></svg>
+            </div>
+          </button>
+        </div>}
       </div>
     </div>
   );
