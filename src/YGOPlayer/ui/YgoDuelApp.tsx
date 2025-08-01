1f6e1b3c697d4ea2abad1c0c67f0d18c6edb2811
Micael Sampaio <ruimicaelsampaio@gmail.com>
2025-08-01 23:18:27 +0100

    feat: add ygo-is-mobile

--- src/YGOPlayer/ui/YgoDuelApp.tsx
+++ src/YGOPlayer/ui/YgoDuelApp.tsx
@@ -2,10 +2,12 @@ import { useEffect, useRef, useState } from "react";
 import { YGODuel } from "../core/YGODuel";
 import { YGOUiController } from "./YGOUiController";
 import { YGOConfig } from "../core/YGOConfig";
+import { useDeviceResolutionInfo } from "../scripts/use-device-resolution-info";
 
 export function YgoDuelApp({ config, bind: onBind, start: onStart }: { bind?: (duel: YGODuel) => void, config: YGOConfig, start?: (duel: YGODuel) => void }) {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [duel, setDuel] = useState<YGODuel>();
+    const { isMobile } = useDeviceResolutionInfo();
 
     useEffect(() => {
         if (!canvasRef.current) return;
@@ -34,7 +36,7 @@ export function YgoDuelApp({ config, bind: onBind, start: onStart }: { bind?: (d
         }
     }, [duel])
 
-    return <div className="ygo-player-core" id="ygo-player-core" {...duel?.mouseEvents.eventsReference}>
+    return <div className={`ygo-player-core ${isMobile ? "ygo-is-mobile" : ""}`} id="ygo-player-core" {...duel?.mouseEvents.eventsReference}>
         <canvas id='ygo-canvas' ref={canvasRef} style={{ width: "100%", height: "100%" }}>
         </canvas>
         {duel && <YGOUiController duel={duel}></YGOUiController>}
