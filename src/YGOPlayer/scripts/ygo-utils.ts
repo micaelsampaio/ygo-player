b94297d89253fa55be000e4248a41d153d3bcfc5
Micael Sampaio <ruimicaelsampaio@gmail.com>
2025-10-11 16:24:37 +0100

    feat: fix mock

--- src/YGOPlayer/scripts/ygo-utils.ts
+++ src/YGOPlayer/scripts/ygo-utils.ts
@@ -50,8 +50,8 @@ export function createFields({ duel, fieldModel }: CreateFieldDto) {
   for (let player = 0; player < 2; ++player) {
     const isPlayer = player === playerIndex;
     const showCards = !!(isPlayer || duel.config.options.showCards)
-    //const controlCards = !!(isPlayer || (showCards && ygo.options.controlOpponentCards))
-    const controlCards = true;
+    const controlCards = !!(isPlayer || (showCards && ygo.options.controlOpponentCards))
+    //const controlCards = true;
     const field = new PlayerField();
     const playerSufix = player === 0 ? "" : "2";
     field.playerIndex = player;
